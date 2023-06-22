51b9ddb63aee45853fdd977a35df17ddc9bab261
DESKTOP-DU85630\bitcamp <jang6129@naver.com>
2023-06-22 18:11:42 +0900

    update

--- src/main/java/com/doma/bookmanagementsystem/controller/BookViewController.java
+++ src/main/java/com/doma/bookmanagementsystem/controller/BookViewController.java
@@ -9,12 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
-public class BlogViewController {
+public class BookViewController {
 
     private final BookService bookService;
 
@@ -34,6 +35,18 @@ public class BlogViewController {
         model.addAttribute("book", new BookViewResponse(book));
 
         return "book";
+    }
+
+    @GetMapping("/new-book")
+    public String newBook(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("book", new BookViewResponse());
+        } else {
+            Book book = bookService.findById(id);
+            model.addAttribute("book", new BookViewResponse(book));
+        }
+
+        return "newBook";
     }
 
 }
--- src/main/resources/static/js/book.js
+++ src/main/resources/static/js/book.js
@@ -3,7 +3,7 @@ const deleteButton = document.getElementById('delete-btn');
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
         let id = document.getElementById('book-id').value;
-        fetch(`/api/books/${id}`, {
+        fetch(`/bms/books/${id}`, {
             method: 'DELETE'
         })
             .then(() => {
@@ -12,3 +12,5 @@ if (deleteButton) {
             });
     });
 }
+
+const modify
